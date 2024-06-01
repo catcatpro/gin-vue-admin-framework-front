d7bdf59e1f97aeaaffc5ad3f716c96ec88c707da
catcatproer <1901749687@qq.com>
2024-06-02 01:59:52 +0800

    引入预加载模块

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/preload'
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
