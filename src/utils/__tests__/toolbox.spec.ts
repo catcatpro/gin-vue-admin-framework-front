99708cb0f7a24c9bb40fb3fdb13adf608d206176
catcatproer <1901749687@qq.com>
2024-06-02 02:00:50 +0800

    添加测试目录

---
+++ src/utils/__tests__/toolbox.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import toolbox from '../toolbox'
+
+describe('getCurrentRouter', () => {
+    it('Get Current Router', () => {
+        // const res = toolbox.getCurrentRouterPath()
+        // if (res)
+        //     expect(res.toString()).toContain('/database_management')
+    })
+})
