278a0a85148095d416c31a0a5e9bd90c9e222fa4
catcatproer <1901749687@qq.com>
2024-06-09 10:16:47 +0800

    too

--- vite.config.ts
+++ vite.config.ts
@@ -24,5 +24,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  test: {
+    deps: {
+      inline: ['element-plus']
+    }
   }
 })
