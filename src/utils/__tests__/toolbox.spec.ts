03b34dab0f6f0c4dff4df12a9e266d49939cd078
catcatproer <1901749687@qq.com>
2024-06-09 10:12:51 +0800

    添加utils单元测试

--- src/utils/__tests__/toolbox.spec.ts
+++ src/utils/__tests__/toolbox.spec.ts
@@ -1,10 +1,29 @@
 import { describe, it, expect } from 'vitest'
+import router from '@/router'
 import toolbox from '../toolbox'
+import { mount, BaseWrapper } from '@vue/test-utils'
+import App from '@/App.vue'
 
-describe('getCurrentRouter', () => {
-    it('Get Current Router', () => {
-        // const res = toolbox.getCurrentRouterPath()
-        // if (res)
-        //     expect(res.toString()).toContain('/database_management')
+describe('toolbox', async () => {
+    const wrapper = mount(
+        App,
+        {
+            global: { plugins: [router] }
+        }
+    )
+    await wrapper.vm.$nextTick()
+
+    it("test is getCurrentRoutePath function", async () => {
+
+        await router.push({ path: '/dashboard' })
+
+        const path: string = toolbox.getCurrentRoutePath()
+        expect(path).toContain('/dashboard')
+    })
+
+    it('test is getCurrentRouteMetaTitle function', async () => {
+        await router.push({ path: '/dashboard' })
+        const title: string = toolbox.getCurrentRouteMetaTitle()
+        expect(title).toContain('仪表盘')
     })
 })
