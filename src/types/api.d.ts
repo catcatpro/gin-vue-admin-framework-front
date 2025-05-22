3bbc62a0966fdbf5e713d1930c6a3424762ea3e2
catcatproer <1901749687@qq.com>
2025-05-22 17:40:48 +0800

    添加添加

---
+++ src/types/api.d.ts
@@ -0,0 +1,16 @@
+
+declare interface BasicResponse{
+    status: HttpStatus
+    msg: string
+    data: Record<never, never>
+}
+
+declare interface GetCaptchaResponse extends BasicResponse{
+    data: Captcha
+}
+
+declare interface Captcha{
+    id: string
+    captcha: string
+}
+
