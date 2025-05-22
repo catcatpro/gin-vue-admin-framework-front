c92551f5ec783929dc9745c66a350c6f6e1d8909
catcatproer <1901749687@qq.com>
2025-05-22 17:41:51 +0800

    添加基于fetch客户端

---
+++ src/utils/ky_http.ts
@@ -0,0 +1,53 @@
+import ky from "ky"
+import type { Options } from 'ky'
+import { token } from "./token"
+import _ from 'lodash'
+import { ElMessage } from "element-plus"
+const { APP_API_URL } = import.meta.env
+
+console.log('eee',import.meta.env.DEV ? '/api' : APP_API_URL)
+const config: Options = {
+  prefixUrl:  import.meta.env.DEV ? '/api' : APP_API_URL, //正式环境用APP_API_URL, //正式环境用APP_API_URL
+  retry: 0,
+  timeout: 15000,
+  headers: {},
+  hooks: {
+    beforeRequest: [
+      (request, options) => {
+        if (token.has()) {
+          request.headers.set('x-header-token', `${token.get()}`);
+        }
+        /**
+ * TOO
+ * 更多操作
+ */
+      }
+    ],
+    afterResponse: [
+      async (request, options, response) => {
+
+        //TOO
+        // 更多操作
+        return response
+
+      }
+    ],
+    beforeError: [
+      async error => {
+        console.error('HTTPError', error);
+
+        // 返回要抛出的 HTTPError 对象
+        ElMessage.error("系统错误：" + error.message)
+        return error;
+      },
+    ]
+  }
+}
+
+const instance = ky.create(config)
+
+
+export default {
+  get: instance.get,
+  post: instance.post
+}
