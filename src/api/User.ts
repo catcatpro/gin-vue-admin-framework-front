fe0fb2ea809cb206cb6bf864e04c1d0437612f08
catcatproer <1901749687@qq.com>
2025-05-28 15:27:01 +0800

    添加登录接口

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,6 +1,28 @@
-import {get, post} from '@/utils/ky_http'
+import ky_http from '@/utils/ky_http'
+import type { ResponsePromise } from 'ky'
+import toolbox from '@/utils/toolbox.ts'
 
-export function Login(username : string, password: string, captcha: Captcha): never{
+export class UserApi{
 
+  /**
+   * 登录接口
+   * @param {string} username
+   * @param {string} password
+   * @param {Captcha} captcha
+   * @constructor
+   * @return { ResponsePromise<LoginResponse> }
+   */
+  static Login(username : string, password: string, captcha: Captcha, auto_login: string): ResponsePromise<LoginResponse>{
+    const sendData = {
+      username,
+      password,
+      captcha: captcha.captcha,
+      captcha_id: captcha.id,
+      auto_login: auto_login,
+    }
+    return ky_http.post<LoginResponse>("public/admin/user/login", {
+      json: sendData,
+    })
 
+  }
 }
