1c68ca6b37ef156bd854e8c7445da215f2f8fbb5
catcatproer <1901749687@qq.com>
2025-05-28 15:25:19 +0800

    添加注释

--- src/api/Coommon.ts
+++ src/api/Coommon.ts
@@ -5,6 +5,10 @@ import type { ResponsePromise } from 'ky'
  * 公共接口
  */
 export class CommonApi{
+    /**
+     * 获取图形验证码
+     * @return ResponsePromise<GetCaptchaResponse>
+     */
     static  getCaptcha() : ResponsePromise<GetCaptchaResponse>{
         return ky_http.get<GetCaptchaResponse>("public/sys/get_captcha")
     }
